dd56ede8fddf99b915b9895a4d8ce1ad4d2b5fea
treveshan.naidoo@gmail.com <treveshan.naidoo@gmail.com>
2024-06-06 23:10:04 +0200

    tests: write tests for Abstract Factory

---
+++ src/DesignPatterns.Creational.Tests/AbstractFactory/AbstractFactoryTests.cs
@@ -0,0 +1,79 @@
+﻿using DesignPatterns.Creational.FactoryMethod.PolicyTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DesignPatterns.Utils.Display;
+using FluentAssertions;
+using NSubstitute;
+using DesignPatterns.Creational.FactoryMethod;
+
+namespace DesignPatterns.Creational.Tests.AbstractFactory
+{
+
+    [TestFixture]
+    public class AbstractFactoryTests
+    {
+        [TestFixture]
+        public class Run
+        {
+            [Test]
+            public void GivenIndividual_ShouldOutputAllIndividualPolicies()
+            {
+                //Arrange
+                var output = Substitute.For<IOutput>();
+                var capturedOutput = new List<string>();
+                output.Display(Arg.Do<string>(s => capturedOutput.Add(s)));
+
+                var sut = CreateAbstractFactory(output);
+                var policyType = "Individual";
+
+                //Act
+                sut.Run(policyType);
+
+                //Assert
+
+                var expected = new List<string>
+                {
+                    "Individual Health Insurance Policy Details",
+                    "Individual Car Insurance Policy Details",
+                    "Individual Home Insurance Policy Details",
+                };
+                capturedOutput.Should().BeEquivalentTo(expected);
+            }
+
+            [Test]
+            public void GivenCorporate_ShouldOutputAllIndividualPolicies()
+            {
+                //Arrange
+                var output = Substitute.For<IOutput>();
+                var capturedOutput = new List<string>();
+                output.Display(Arg.Do<string>(s => capturedOutput.Add(s)));
+
+                var sut = CreateAbstractFactory(output);
+                var policyType = "Corporate";
+
+                //Act
+                sut.Run(policyType);
+
+                //Assert
+
+                var expected = new List<string>
+                {
+                    "Corporate Health Insurance Policy Details",
+                    "Corporate Car Insurance Policy Details",
+                    "Corporate Home Insurance Policy Details",
+                };
+                capturedOutput.Should().BeEquivalentTo(expected);
+            }
+
+            private Creational.FactoryMethod.AbstractFactory CreateAbstractFactory(IOutput output)
+            {
+                return new Creational.FactoryMethod.AbstractFactory(output);
+            }
+        }
+
+
+    }
+}
